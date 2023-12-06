a78d3361c4db8e851258c2e55854c915b99f362c
AnneliesVlaar <Annelies_Vlaar@hotmail.com>
2023-12-06 17:40:06 +0100

    show week number and current date

--- src/skrop/app.py
+++ src/skrop/app.py
@@ -5,6 +5,16 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
+import datetime
+
+def get_week_number():
+    week_number = datetime.date.today().isocalendar()[1]
+    return f"This is week: {week_number}."
+
+def get_today():
+    today = datetime.date.today()
+    return f"Today is: {today}."
+
 
 class Skrop(toga.App):
 
@@ -22,6 +32,15 @@ class Skrop(toga.App):
         self.main_window.content = main_box
         self.main_window.show()
 
+        date_box = toga.Box(style=Pack(direction=COLUMN, padding=5))
+
+        week_label = toga.Label(get_week_number(), style=Pack(padding=(0, 5)))
+        date_label = toga.Label(get_today(), style=Pack(padding=(0, 5)))
+
+        date_box.add(week_label)
+        date_box.add(date_label)
+
+        main_box.add(date_box)
 
 def main():
     return Skrop()
